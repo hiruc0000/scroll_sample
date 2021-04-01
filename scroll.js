d44a69cf9209d4196f07f0a6865ce622035fe34c
hiruc0000 <81366454+hiruc0000@users.noreply.github.com>
2021-04-01 22:57:08 +0900

    sample01

---
+++ scroll.js
@@ -0,0 +1,31 @@
+(() => {
+  const scrollTrigger = document.querySelectorAll('a[href^="#"]');
+
+  for (let i = 0; i < scrollTrigger.length; i++) {
+    scrollTrigger[i].addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const href = scrollTrigger[i].getAttribute('href');
+      const targetElement = document.querySelector(href);
+
+      const rect = targetElement.getBoundingClientRect().top;
+      const offset = pageYOffset;
+      const gap = 53;
+      const target = rect + offset - gap;
+
+      scrollTo({
+        top: target,
+        behavior: 'smooth'
+      });
+    });
+  }
+
+  //TOPへ戻るボタンへのイベント
+  const toTop = document.querySelector('.to-top');
+  toTop.addEventListener('click', () => {
+    scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+})();
